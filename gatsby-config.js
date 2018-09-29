48189939a3bda6edc9e459e789e8862503ca2bd9
Emmanuel Churchill <churchillemmanuel59@gmail.com>
2018-09-29 20:46:44 +0100

    added keywords and description metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.',
+    keywords: 'react course, react for designers'
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '4lx5ac5q6pys',
+        accessToken: '58165d649f83eabb0f5cd3da9125bba0d6b2d0543e5df16e1f2f9c88cfec3ebe'
+      }
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
